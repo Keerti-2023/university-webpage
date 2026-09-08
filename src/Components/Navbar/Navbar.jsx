f58304a8ca99fe45e992719a70c5a32e64c72489
Sneha Pagare <154441020+snehapagare1212@users.noreply.github.com>
2026-09-08 17:39:55 +0530

    Fix navbar links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
-
-import './Navbar.css';
-import logo from '../../assets/logo.png';
-import { Link } from 'react-scroll';
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [bg, setBg] = useState(false);
   const [username, setUsername] = useState("");
+
+  // GitHub Pages base path
+  const baseUrl = import.meta.env.BASE_URL;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,52 +34,90 @@ const Navbar = () => {
 
     alert("Logged out successfully!");
 
-    window.location.href = `${import.meta.env.BASE_URL}Login.html`;
+    window.location.href = `${baseUrl}Login.html`;
   };
 
   return (
     <nav className={`container ${bg ? "darknav" : ""}`}>
 
-      <img src={logo} alt="Logo" className="logo" />
+      {/* Logo */}
+      <img src={logo} alt="University Logo" className="logo" />
 
       <ul>
 
+        {/* Home */}
         <li>
-          <Link to="hero" smooth={true} offset={0} duration={500}>
+          <Link
+            to="hero"
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
             Home
           </Link>
         </li>
 
+        {/* Programs */}
         <li>
-          <Link to="programs" smooth={true} offset={-280} duration={500}>
+          <Link
+            to="programs"
+            smooth={true}
+            offset={-280}
+            duration={500}
+          >
             Programs
           </Link>
         </li>
 
+        {/* About */}
         <li>
-          <Link to="about" smooth={true} offset={-160} duration={500}>
+          <Link
+            to="about"
+            smooth={true}
+            offset={-160}
+            duration={500}
+          >
             About
           </Link>
         </li>
 
+        {/* Gallery */}
         <li>
-          <Link to="gallery" smooth={true} offset={-290} duration={500}>
+          <Link
+            to="gallery"
+            smooth={true}
+            offset={-290}
+            duration={500}
+          >
             Gallery
           </Link>
         </li>
 
+        {/* Testimony */}
         <li>
-          <Link to="testimony" smooth={true} offset={-290} duration={500}>
+          <Link
+            to="testimony"
+            smooth={true}
+            offset={-290}
+            duration={500}
+          >
             Testimony
           </Link>
         </li>
 
+        {/* Contact */}
         <li>
-          <Link to="contact" smooth={true} offset={-250} duration={500}>
+          <Link
+            to="contact"
+            smooth={true}
+            offset={-250}
+            duration={500}
+          >
             Contact
           </Link>
         </li>
 
+        {/* Logged In */}
         {username ? (
           <>
             <li className="welcome-text">
@@ -94,9 +134,10 @@ const Navbar = () => {
             </li>
           </>
         ) : (
+          /* Not Logged In */
           <>
             <li>
-              <a href={`${import.meta.env.BASE_URL}Signup.html`}>
+              <a href={`${baseUrl}Signup.html`}>
                 <button className="btn">
                   Sign Up
                 </button>
@@ -104,7 +145,7 @@ const Navbar = () => {
             </li>
 
             <li>
-              <a href={`${import.meta.env.BASE_URL}Login.html`}>
+              <a href={`${baseUrl}Login.html`}>
                 <button className="btn">
                   Login
                 </button>
